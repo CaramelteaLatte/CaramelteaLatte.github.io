285b05f87a1ea54867cbc8107d4abb4c8c6bd57a
CaramelteaLatte <cyx2691264332@gmail.com>
2024-09-08 18:19:10 +0800

    edit

--- docs/py/new.py
+++ docs/py/new.py
@@ -1,1 +1,14 @@
-print(bin(31))
+b=[1,2,3]
+b[2]=b
+s = 0
+print(b)
+for row in b:
+    print(row)
+    if type(row)==list:
+        print(row)
+        for elem in row:
+            print(elem)
+            s += elem
+    else:
+        s+=row
+print(s)
